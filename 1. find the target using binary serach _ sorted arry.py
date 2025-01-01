15d8a1c4a20af0f2232807c96cb7a7535b588b73
Aadil-1245 <135581341+Aadil-1245@users.noreply.github.com>
2025-01-01 22:10:18 +0530

    Add files via upload

---
+++ 1. find the target using binary serach _ sorted arry.py
@@ -0,0 +1,18 @@
+def fun(arr,n,target):
+    low = 0 
+    n = len(arr)
+    high = n -1 
+    while(low <= high):
+        mid = low + high // 2 
+        if (arr[mid] == target):
+            return mid
+        elif(target > arr[mid]):
+            low = mid +1 
+        else:
+            high = mid -1 
+        return -1 
+arr = [ -1 , 2, 5, 7, 7, 9]
+target = 5
+n = len(arr)
+res = fun(arr,n,target)
+print(res)
